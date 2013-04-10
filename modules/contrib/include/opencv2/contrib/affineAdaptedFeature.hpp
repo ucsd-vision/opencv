049dc258d98370820ae6c6a7c33731e1e4e4058b
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2013-04-09 17:31:19 -0700

    Revert "moved asift to nonfree"
    This reverts commit 60cf78d6ce30c7345d43fde01d7ad43338f42e56.

--- modules/contrib/include/opencv2/contrib/affineAdaptedFeature.hpp
+++ modules/contrib/include/opencv2/contrib/affineAdaptedFeature.hpp
@@ -21,6 +21,37 @@ namespace cv {
 
 using namespace std;
 
+class AffineAdaptedFeature2D: public cv::Feature2D {
+public:
+	AffineAdaptedFeature2D(const cv::Ptr<cv::Feature2D>& feature2d);
+	AffineAdaptedFeature2D(const cv::Ptr<cv::FeatureDetector>& featureDetector,
+			const cv::Ptr<cv::DescriptorExtractor>& descriptorExtractor);
+
+	virtual int descriptorSize() const;
+	virtual int descriptorType() const;
+
+	virtual void operator()(cv::InputArray image, cv::InputArray mask,
+			std::vector<cv::KeyPoint>& keypoints, cv::OutputArray descriptors,
+			bool useProvidedKeypoints = false) const;
+
+protected:
+	void initialize();
+
+	void detectAndComputeImpl(const cv::Mat& image, const cv::Mat& mask,
+			std::vector<cv::KeyPoint>& keypoints, cv::Mat& descriptors) const;
+
+	void computeImpl(const cv::Mat& image, std::vector<cv::KeyPoint>& keypoints,
+			cv::Mat& descriptors) const;
+	void detectImpl(const cv::Mat& image, std::vector<cv::KeyPoint>& keypoints,
+			const cv::Mat& mask = cv::Mat()) const;
+
+	cv::Ptr<cv::Feature2D> feature2d;
+	cv::Ptr<cv::FeatureDetector> featureDetector;
+	cv::Ptr<cv::DescriptorExtractor> descriptorExtractor;
+
+	std::vector<cv::Vec2f> affineTransformParams;
+};
+
 CV_WRAP void detectAndExtractDescriptorsASIFT(
 		const Mat& image,
 		vector<KeyPoint>& keyPoints,
